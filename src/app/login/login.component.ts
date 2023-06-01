9c908d689a39c3e93b3359b713dfc5ed8d9cbfd7
Leothomasv <leothomasv@unitec.edu>
2023-06-01 00:11:23 -0600

    mini cambios

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,14 +10,14 @@ export class LoginComponent implements OnInit {
 
   username: string;
   password: string;
-  isloggedIn: boolean = true;
+  isloggedIn = true;
   constructor(private authService: AuthService,private router: Router) { }
 
   login() {
     const response = this.authService.login(this.username, this.password);
     if(response){
       console.log('Usuario correcto!');
-      this.router.navigate(['/tabs/images'])
+      this.router.navigate(['/tabs/images']);
     }else{
       this.isloggedIn = false;
       console.log('Usuario no valido');
